0ec8606b535ec5e191d24f37b814248fe406fbc7
shriramt124 <shriramt.124@gmail.com>
2024-10-02 15:46:28 +0530

    "CSES TWO_SET_DP PROBLEM SOLVED USING MEMOIZATION AND TABULATION BOTH"

---
+++ common.h
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+#include<vector>
+#include<string>
+#include<stack>
+#include<queue>
+#include<map>
+#include<unordered_map>
+#include<set>
+#include<unordered_set>
+#include<algorithm>
+#include<cmath>
+#define ll long long
+#define ull unsigned long long
+#define cint const int&
+#define Pi 3.141592653589793
+
+#define pii pair<int,int>
+#define vi vector<ll>
+#define vvi vector<vector<ll>>
+#define vpi vector<pair<ll,ll>>
+#define vvpi vector<vector<pair<ll,ll>>>
+#define vpii vector<pair<int,int>>
+#define vvpii vector<vector<pair<int,int>>>
+
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define per(i,a,b) for(int i=a;i>=b;i--)
+#define pb push_back
+#define mp make_pair
+#define fi first
+#define se second
+#define all(x) x.begin(),x.end()
+#define sz(x) (int)(x).size()
+
+#define endl '\n'
+const int MOD = 1e9+7;
